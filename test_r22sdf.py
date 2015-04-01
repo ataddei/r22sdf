8dc666ef1d2a3e162259df8f3c52494430563e38
Alfredo Taddei <alfredo.taddei@clariphy.com.ar>
2015-04-01 11:55:14 -0300

    Test now work after fixing race conditions, long random values fails occasionnaly because of double precission between numpy and R22SDF

--- test_r22sdf.py
+++ test_r22sdf.py
@@ -4,15 +4,6 @@ from numpy import *
 from r22sdf import *
 import unittest
 from random import *
-## d=Signal(complex(1,0))
-## a=Signal(complex(1,0))
-## reset=ResetSignal(1,1,False)
-## clock = Signal(bool(0))
-## @always(delay(1))
-## def clkgen():
-##     clock.next = not clock
-##     return clkgen
-
 
 class TestDefault(unittest.TestCase):
 
@@ -26,6 +17,17 @@ class TestDefault(unittest.TestCase):
         self.clock = Signal(bool(0))
         self.uut = stage(self.a,self.reset,self.clock,self.d,N=1)
         self.gg=self.input_generator()
+    def tearDown(self):
+        self.N       =[]
+        self.latency =[]
+        self.collect =[]
+        self.a       =[]
+        self.d       =[]
+        self.reset   =[]
+        self.clock   =[]
+        self.uut     =[]
+        self.gg      =[]
+
     def input_generator(self):
         self.inputs=[complex(1,0) for i in range(self.N)]*2
         for i in self.inputs:
@@ -44,24 +46,58 @@ class TestDefault(unittest.TestCase):
                 yield delay(1)
             yield delay(1)
             self.reset.next=False
-            while True:
-                if (self.reset==False and self.clock==True):
-                    self.a.next=next(self.gg)
-                    self.collect.append(self.d.val)                    
+            '''Driving stimulus in positive cycle and strobing in negative cycle to avoid race coinditions'''
+            while True:                        
                 yield delay(1)
-        sim=Simulation(self.uut,clkgen,stimulus)
+        @always(self.clock.posedge)
+        def stim():
+            if (self.reset==False):
+                self.a.next= next(self.gg)
+        @always(self.clock.negedge)
+        def strobe():
+            if (self.reset==False):
+                self.collect.append(self.d.val)
+        
+                
+        sim=Simulation(self.uut,clkgen,stimulus,stim,strobe)
         for j in range(20):
             sim.run(1,quiet=1)
         self.check()
     def check(self):
-        self.assertListEqual(list(fft.fft(self.inputs[0:self.N])),self.collect[self.latency-1:self.latency-1+self.N])
+        '''Checks sorted values'''
+        self.assertListEqual(list(sort(fft.fft(self.inputs[0:self.N]))),list(sort(self.collect[self.latency-1:self.latency-1+self.N])))
 
 class TestDefaultRandomReal(TestDefault):
     def input_generator(self):
-        self.inputs=[complex(random(),random()) for i in range(self.N)]*2
+        self.inputs=[complex(random(),0) for i in range(self.N)]*2
         for i in self.inputs:
             yield i
 
+class TestDefaultZero(TestDefault):
+    def input_generator(self):
+        self.inputs=[complex(0,0) for i in range(self.N)]*2
+        for i in self.inputs:
+            yield i
+class TestDefaultImpulse0(TestDefault):
+    def input_generator(self):
+        self.inputs=[complex(random(),random()) if i==0 else complex(0,0) for i in range(self.N)]*2
+        for i in self.inputs:
+            yield i
+class TestDefaultImpulse1(TestDefault):
+    def input_generator(self):
+        self.inputs=[complex(random(),random()) if i==1 else complex(0,0) for i in range(self.N)]*2
+        for i in self.inputs:
+            yield i
+class TestDefaultImpulse2(TestDefault):
+    def input_generator(self):
+        self.inputs=[complex(1,0) if i==2 else complex(0,0) for i in range(self.N)]*2
+        for i in self.inputs:
+            yield i
+class TestDefaultImpulse3(TestDefault):
+    def input_generator(self):
+        self.inputs=[complex(random(),0) if i==3 else complex(0,0) for i in range(self.N)]*2
+        for i in self.inputs:
+            yield i
 
 if __name__ == '__main__':
     unittest.main()
