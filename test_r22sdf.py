48f9d7350cac67b3c4f7ae3da0d5006896a9abd7
Alfredo Taddei <alfredo.taddei@clariphy.com.ar>
2015-04-14 15:36:41 -0300

    integrated all tests again and put anopther option for trace signalsbut not working yet

--- test_r22sdf.py
+++ test_r22sdf.py
@@ -42,8 +42,8 @@ class TestDefault(unittest.TestCase):
         global Ntest
 
         def _test():   
-            uut = r22sdf_top(self.a,self.reset,self.clock,self.d,N=1)
-
+            #self.uut = r22sdf_top(self.a,self.reset,self.clock,self.d,N=1)
+            uut = self.uut
             @always(delay(1))
             def clkgen():
                 self.clock.next = not self.clock        
@@ -183,12 +183,58 @@ class TestDefaultFFTImpulse3(TestDefaultFFT):
             yield i
 class TestDefaultFFTImpulseRandom(TestDefaultFFT):
     def input_generator(self):
-        imp=randint(0,self.N-1)
+        imp=[randint(0,self.N-1), randint(0,self.N-1) ]
+        imp=[3,4]
         print imp
-        self.inputs=[complex(random(),0) if i==imp else complex(0,0) for i in range(self.N)]*2
+        self.inputs=[complex(1,0) if i in imp else complex(0,0) for i in range(self.N)]*2
         for i in self.inputs:
             yield i
-             
+class TestDefaultFFTRandom(TestDefaultFFT):
+    def input_generator(self):
+        self.inputs=[complex128(complex(round(random()-0.5,5),round(random()-0.5,5))).round(8) for i in range(self.N)]*2
+        for i in self.inputs:
+            yield i
+
+class TestDefaultFFTSaw(TestDefaultFFT):
+    def input_generator(self):
+        self.inputs=[complex128(complex(i,i)).round(8) for i in range(self.N)]*2
+        for i in self.inputs:
+            yield i
+
+class TestDefaultFFTRandomImpulseSweep(TestDefaultFFTImpulseRandom):
+    def setUp(self,idx=0):
+        self.N=16
+        self.latency=self.N-1
+        self.collect=[]
+        self.a=Signal(complex(0,0))
+        self.d=Signal(complex(0,0))
+        self.reset=ResetSignal(1,1,False)
+        self.clock = Signal(bool(0))
+        self.uut = r22sdf_top(self.a,self.reset,self.clock,self.d,N=2)
+        self.idx=idx
+        self.inputs=[complex(1,0) if i == idx else complex(0,0) for i in range(self.N)]*2
+        self.gg=self.input_generator()
+
+    def input_generator(self):        
+        for i in self.inputs:
+            yield i
+    def runTest(self):
+        self.setUp()
+        self.count=[]
+        for i in range(16):
+            try:
+                self.setUp(i)
+                super(TestDefaultFFTRandomImpulseSweep,self).runTest()
+                self.tearDown()
+            except Exception as e:
+                #print e,"Exception Failed in: ",self.idx
+                self.count.append(i)
+        if len(self.count)>0:
+            print "Failing impulses: ",self.count
+            raise self.failureException
+                
+
+
 # creating a new test suite
 FFT16Suite = unittest.TestSuite()
  
@@ -199,6 +245,7 @@ FFT16Suite.addTest(unittest.makeSuite(TestDefaultFFTImpulse1))
 FFT16Suite.addTest(unittest.makeSuite(TestDefaultFFTImpulse2))
 FFT16Suite.addTest(unittest.makeSuite(TestDefaultFFTImpulse3))
 FFT16Suite.addTest(unittest.makeSuite(TestDefaultFFTImpulseRandom))
+FFT16Suite.addTest(unittest.makeSuite(TestDefaultFFTRandom))
 FFT16Suite.addTest(unittest.makeSuite(TestDefaultFFTZero))
 if __name__ == '__main__':
     unittest.main()
